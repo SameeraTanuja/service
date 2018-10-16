56d0ba431f5161bea09810a2708101015855c9e2
lpilaka <lpilaka@IN-MUM-LDVM022.corp.capgemini.com>
2018-10-16 08:46:42 -0400

    commit

---
+++ src/main/java/com/capgemini/configservice/ConfigserviceApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+}
